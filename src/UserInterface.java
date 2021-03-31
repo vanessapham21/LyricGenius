967aa99af2c6662a6d6d9dad4041da94b79d215c
vanessapham21 <77404949+vanessapham21@users.noreply.github.com>
2021-03-31 12:32:12 -0700

    Add JSON Parse
    + jsonParse class

--- src/UserInterface.java
+++ src/UserInterface.java
@@ -1,2 +1,4 @@
-public class UserInterface {
+public class UserInterface
+{
+
 }
---
+++ src/jsonParse.java
@@ -0,0 +1,6 @@
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.*;
+
+public class jsonParse {
+}
